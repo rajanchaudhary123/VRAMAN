9840ecf2c267987d8fe2b374e0337f3cdfb73b28
Rajan <chaudharyrajan903@gmail.com>
2023-05-23 16:28:33 +0545

    activating user,vendor and password reset

--- bhraman_main/settings.py
+++ bhraman_main/settings.py
@@ -150,4 +150,5 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'BHRAMAN marketplace <bhraman653@gmail.com>'
 
