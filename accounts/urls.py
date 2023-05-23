4cb31f0c10cbeb41d178f9fcb9f2aa0338f4bcf7
SwtVer <swativerma5050@gmail.com>
2023-05-23 20:53:12 +0545

    test

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.myAccount),
+    
     path('registerUser/', views.registerUser,name='registerUser'),
     path('registerVendor/', views.registerVendor,name='registerVendor'),
     
