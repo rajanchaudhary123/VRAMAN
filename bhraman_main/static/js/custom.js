1f2ace5d6ec0019c5ac83a9edcc53178e0557f21
SwtVer <swativerma5050@gmail.com>
2023-05-29 10:26:56 +0545

    section 12 110 complete

---
+++ bhraman_main/static/js/custom.js
@@ -0,0 +1,31 @@
+let autocomplete;
+
+function initAutoComplete(){
+autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById('id_address'),
+    {
+        types: ['geocode', 'establishment'],
+        //default in this app is "IN" - add your country code
+        componentRestrictions: {'country': ['in','np']},
+    })
+// function to specify what should happen when the prediction is clicked
+autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged (){
+    var place = autocomplete.getPlace();
+
+    // User did not select the prediction. Reset the input field or alert()
+    if (!place.geometry){
+        document.getElementById('id_address').placeholder = "Start typing...";
+    }
+    else{
+         console.log('place name=>', place.name)
+    }
+
+    //get address component and assign them in the field
+    //console.log(place);
+    var geocoder = new google.maps.Geocoder()
+    var address = document.getElementById('id_address').value 
+
+}
