ab89bb3e0bc4cfce478b3ca59ad2d02afee42f8b
Rajan <chaudharyrajan903@gmail.com>
2023-05-29 18:43:03 +0545

    review and rating

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.marketplace, name='marketplace'),
     
     path('<slug:vendor_slug>/',views.vendor_detail, name='vendor_detail'),
-    path('marketplace/review_rating/',views.review_rating, name='review_rating'),
+    #path('marketplace/review_rating/',views.review_rating, name='review_rating'),
+    path('submit_review/<int:vendor_id>/',views.submit_review,name='submit_review'),
 
     # ADD TO CART
     #path('add_to_cart/<int:food_id>/', views.add_to_cart, name='add_to_cart'),
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render , redirect
 from vendor.models import  Vendor
 from menu.models import Category, PackageItem
 from django.db.models import Prefetch
+from .forms import ReviewForm
+from django.contrib import messages
+from .models import ReviewRating
+from django.contrib.auth.decorators import login_required,user_passes_test
+from accounts.views import check_role_customer
 
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -28,8 +33,32 @@ def vendor_detail(request, vendor_slug):
 
     return render(request, 'marketplace/vendor_detail.html',context)
 
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def submit_review(request, vendor_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == "POST":
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, vendor__id =vendor_id)
+            form = ReviewForm(request.POST,instance=reviews)
+            form.save()
+            messages.success(request,"Review has been updated.")
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.vendor_id = vendor_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, 'Thank you! Your review has been submitted.')
+                return redirect(url)
 
-def review_rating(request):
+
+       
         
-
-    return render(request, 'marketplace/review_rating.html')
+    
