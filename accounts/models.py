0e843a3f8138533605200d8426a283af3a8f25ee
SwtVer <swativerma5050@gmail.com>
2023-05-29 13:39:02 +0545

    issue in vprofile solved

--- accounts/models.py
+++ accounts/models.py
@@ -102,7 +102,7 @@ class UserProfile(models.Model):
     address = models.CharField(max_length=250, blank=True, null=True)
     
     country = models.CharField(max_length=15, blank=True, null=True)
-    state = models.CharField(max_length=15, blank=True, null=True)
+    state = models.CharField(max_length=20, blank=True, null=True)
     city = models.CharField(max_length=15, blank=True, null=True)
     pin_code = models.CharField(max_length=6, blank=True, null=True)
     latitude = models.CharField(max_length=20, blank=True, null=True)
