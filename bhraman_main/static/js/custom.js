81a1546255b561e03de421510e79791b6341caab
SwtVer <swativerma5050@gmail.com>
2023-06-06 23:29:37 +0545

    tax module

--- bhraman_main/static/js/custom.js
+++ bhraman_main/static/js/custom.js
@@ -199,22 +199,21 @@ $(document).ready(function () {
     }
   }
 
-  // apply cart amounts
-  function applyCartAmounts(subtotal, tax_dict, grand_total){
-    if(window.location.pathname == '/cart/'){
-        $('#subtotal').html(subtotal)
-        $('#tax').html(tax)
-        $('#total').html(grand_total)
+ // apply cart amounts
+ function applyCartAmounts(subtotal, tax_dict, grand_total){
+  if(window.location.pathname == '/cart/'){
+      $('#subtotal').html(subtotal)
+      $('#total').html(grand_total)
 
-        console.log(tax_dict)
-        for(key1 in tax_dict){
-            console.log(tax_dict[key1])
-            for(key2 in tax_dict[key1]){
-                // console.log(tax_dict[key1][key2])
-                $('#tax-'+key1).html(tax_dict[key1][key2])
-            }
-        }
-    }
+      console.log(tax_dict)
+      for(key1 in tax_dict){
+          console.log(tax_dict[key1])
+          for(key2 in tax_dict[key1]){
+              // console.log(tax_dict[key1][key2])
+              $('#tax-'+key1).html(tax_dict[key1][key2])
+          }
+      }
+  }
 }
 
 });
--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import ReviewRating,ReviewRatingPackage
-from .models import Cart
+from .models import Cart,Tax
 
 
 
 
 class CartAdmin(admin.ModelAdmin):
     list_display = ('user', 'packageitem', 'quantity', 'updated_at')
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('tax_type', 'tax_percentage', 'is_active')
 
 # Register your models here.
 admin.site.register(ReviewRating)
@@ -14,4 +16,6 @@ admin.site.register(ReviewRating)
 admin.site.register(Cart, CartAdmin)
 
 admin.site.register(ReviewRatingPackage)
+
+admin.site.register(Tax, TaxAdmin)
 
